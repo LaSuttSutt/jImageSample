83385c77a423e54235a4b41479a584a86cec0ccf
LaSuttSutt <sven.sutter@hotmail.de>
2015-07-20 11:28:54 +0200

    Initial API-Setup

---
+++ api/src/main/java/ch/nag/ImageSample/api/ImageSampleApi.java
@@ -0,0 +1,8 @@
+package ch.nag.ImageSample.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class ImageSampleApi extends Application {
+}
