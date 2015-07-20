53c56bd1276ed60d91af97cc4b873f774fd69503
LaSuttSutt <sven.sutter@hotmail.de>
2015-07-20 15:46:15 +0200

    Initial API-Setup

--- api/src/main/java/ch/nag/ImageSample/api/common/CommonApi.java
+++ api/src/main/java/ch/nag/ImageSample/api/common/CommonApi.java
@@ -3,6 +3,8 @@ package ch.nag.ImageSample.api.common;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import java.io.File;
+import java.net.URL;
 
 @Path("/common")
 public class CommonApi {
@@ -15,8 +17,18 @@ public class CommonApi {
 
     @GET
     @Path("/getImage")
-    public byte[] getImage() {
+    public File getImage() {
 
-        return null;
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        URL url = classLoader.getResource("files/TUS.png");
+        if (url == null)
+            return null;
+
+        String loadedFileString = url.getFile();
+        if (loadedFileString == null)
+            return null;
+
+        return new File(loadedFileString);
     }
 }
