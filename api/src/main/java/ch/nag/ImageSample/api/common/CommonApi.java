77733e4010ff94616a94a716ff394d03c0a892cf
LaSuttSutt <sven.sutter@hotmail.de>
2015-08-26 20:38:53 +0200

    Image

--- api/src/main/java/ch/nag/ImageSample/api/common/CommonApi.java
+++ api/src/main/java/ch/nag/ImageSample/api/common/CommonApi.java
@@ -32,15 +32,26 @@ public class CommonApi {
         BufferedImage bImageFromConvert = ImageIO.read(in);
         in.close();
 
-        Image scaledImage = bImageFromConvert.getScaledInstance(imageTransfer.getClientWidth(), imageTransfer.getClientHeight(), Image.SCALE_AREA_AVERAGING);
+        Image scaledImage = bImageFromConvert.getScaledInstance
+                (imageTransfer.getClientWidth(), imageTransfer.getClientHeight(), Image.SCALE_AREA_AVERAGING);
 
-        BufferedImage bufferedThumbnail = new BufferedImage(imageTransfer.getCutWidth(), imageTransfer.getCutHeight(), BufferedImage.TYPE_INT_RGB);
-        bufferedThumbnail.getGraphics().drawImage(scaledImage, 0, 0, imageTransfer.getCutWidth(), imageTransfer.getCutHeight(),
-                imageTransfer.getCutLeft(), imageTransfer.getCutTop(), imageTransfer.getCutWidth() + imageTransfer.getCutLeft(),
-                imageTransfer.getCutTop() + imageTransfer.getCutHeight(), Color.WHITE, null);
+        BufferedImage bufferedThumbnail = new BufferedImage
+                (imageTransfer.getCutWidth(), imageTransfer.getCutHeight(), BufferedImage.TYPE_INT_ARGB);
+
+        //Graphics2D result = bufferedThumbnail.createGraphics();
+        //result.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR, 0.0f));
+        //result.setColor(new Color(0, 0, 0, 0));
+        //result.fillRect(0, 0, bufferedThumbnail.getWidth(), bufferedThumbnail.getHeight());
+        //result.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
+
+        bufferedThumbnail.getGraphics().drawImage(scaledImage, 0, 0,
+                imageTransfer.getCutWidth(), imageTransfer.getCutHeight(),
+                imageTransfer.getCutLeft(), imageTransfer.getCutTop(),
+                imageTransfer.getCutWidth() + imageTransfer.getCutLeft(),
+                imageTransfer.getCutTop() + imageTransfer.getCutHeight(), null);
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(bufferedThumbnail, "jpeg", baos);
+        ImageIO.write(bufferedThumbnail, "png", baos);
         baos.flush();
         commonLogic.storeImage(baos.toByteArray());
         baos.close();
